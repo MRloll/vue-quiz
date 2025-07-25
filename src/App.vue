82279665ed00c34cd6d04c2d1bd6220fc4b91134
MrLoll <walidelloll2018@gmail.com>
2025-07-25 17:08:02 +0300

    Init: structure & plugins
    - set folder structure
    - install i18n localization
    - install tailwind
    - set dark mode

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+  import { onMounted } from "vue";
+  import { useI18n } from "vue-i18n";
+  const savedLocale = localStorage.getItem("lang") || "en";
+
+  const { locale } = useI18n();
+
+  const setLang = (lang = "en") => {
+    locale.value = lang;
+    localStorage.setItem("lang", lang);
+    document.documentElement.lang = lang;
+    document.documentElement.dir = lang === "ar" ? "rtl" : "ltr";
+  };
+
+  function changeLang(lang) {
+    setLang(lang);
+  }
+
+  onMounted(() => {
+    setLang(savedLocale);
+  });
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">{{ $t("hello") }}</h1>
+    <button class="btn" @click="changeLang('ar')">عربي</button>
+    <button class="btn" @click="changeLang('en')">English</button>
+  </div>
+</template>
+
+<style scoped></style>
